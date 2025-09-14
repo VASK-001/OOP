2a24e240eee2402ade2d545e4d0663c031251fdb
VASK-001 <vadim.skrygin@gmail.com>
2025-09-14 22:04:10 +0300

    HW - OB03 - All+Extra

--- Zoo_corp.py
+++ Zoo_corp.py
@@ -1,9 +1,40 @@
+import pickle
+
 class Zoo_Aggr:
     def __init__(self, name, address):
         self.name = name
         self.address = address
         self.animals = []
         self.attendants = []
+
+#***************** Save & Retrieve the Zoo ************************************************
+    def save_zoo(self):
+        """сохранение в: zoo_state.pkl (binary) и zoo_info.txt для людей"""
+        # 1. Для программы (бинарный pickle)
+        with open('zoo_state.pkl', 'wb') as f:
+            pickle.dump(self, f)
+
+        # 2. Для людей (txt)
+        with open('zoo_info.txt', 'w', encoding='utf-8') as f:
+            f.write(f"Зоопарк: {self.name}\n")
+            f.write(f"Адрес: {self.address}\n")
+            f.write(f"Животных: {len(self.animals)}\n")
+            f.write(f"Сотрудников: {len(self.attendants)}\n\n")
+
+            f.write("СПИСОК ЖИВОТНЫХ:\n")
+            for animal in self.animals:
+                f.write(f"- {animal.name} ({animal.species}), {animal.age} лет\n")
+
+            f.write("\nСПИСОК СОТРУДНИКОВ:\n")
+            for attendant in self.attendants:
+                f.write(f"- {attendant.name}: {attendant.position}, {attendant.pay} руб.\n")
+
+    @classmethod
+    def load_zoo(cls):
+        """Загрузка прошлого зоопарка из zoo_state.pkl"""
+        with open('zoo_state.pkl', 'rb') as f:
+            return pickle.load(f)
+#********************************************************************************************
 
     class ZooAnimal:  # Вложенный класс - Э тока внутри Zoo_Aggr
         def __init__(self, zoo, name, species, age):
@@ -12,6 +43,9 @@ class Zoo_Aggr:
             self.species = species
             self.age = age
 
+        def __repr__(self):  # ← метод форматирования при выводе списка
+            return f"{self.name} ({self.species}) {self.age} лет\n"
+
     class ZooAttendant:
         def __init__(self, zoo, name, position, pay):
             self.zoo = zoo
@@ -19,9 +53,9 @@ class Zoo_Aggr:
             self.position = position
             self.pay = pay
 
-        def __repr__(self):  # ← этот метод
+        def __repr__(self):  # ← тоже
             return f"{self.name} ({self.position}) з/п: {self.pay} руб.\n"
-    #*************************************************************
+#*************************************************************
 
     def add_animal(self, name, species, age):
         animal = self.ZooAnimal(self, name, species, age)
@@ -59,8 +93,21 @@ class Zoo_Aggr:
             print(f'{self.name} лечит больное животное')
 
 #===================== Testing =====================================
+
+# Загружаем сохраненное сост-е
+the_zoo = Zoo_Aggr.load_zoo()
+
+# Проверяем:
+print(f"Загружен зоопарк: {the_zoo.name}")
+print(f"Животных: {len(the_zoo.animals)}")
+print(f"Сотрудников: {len(the_zoo.attendants)}")
+
+print(the_zoo.animals)
+print(the_zoo.attendants)
+'''
+
 the_zoo = Zoo_Aggr("Пионерский", 'Пятисобачий пер., 5')
-animal1 = Zoo_Aggr.ZooAnimal(zoo=the_zoo, name='Кеша', species='Птеродактиль', age=2000) # создаёт, но в [] не ++
+animal1 = Zoo_Aggr.ZooAnimal(zoo=the_zoo, name='Кеша', species='Птеродактиль', age=20000) # создаёт, но в [] не ++
 the_zoo.animals.append(animal1)  # - тогда + вот так
 
 the_zoo.add_animal('Зайка','зайки', 1 )
@@ -83,3 +130,5 @@ vet.heeal_animal()
 print(the_zoo.attendants)
 for attendant in the_zoo.attendants:
     print(attendant.name,'\t', attendant.position,'\t', attendant.pay)
+'''
+the_zoo.save_zoo()
