649948c33c9739a87de06c1592004a8146583f47
VASK-001 <vadim.skrygin@gmail.com>
2025-09-14 13:51:31 +0300

    Zoo - BC Animal added

---
+++ Zoo.py
@@ -0,0 +1,13 @@
+# Программа Zoo
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def make_sound(self):
+        print('Ж издаёт характерный звук')
+
+    def eat(self):
+        print('Ж ест')
+
+
